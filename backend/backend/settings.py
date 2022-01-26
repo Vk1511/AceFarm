e5671cc3d1beeef5311727294c865bd6dbb75bc4
vk1511 <1511vishwkadu@gmail.com>
2022-01-26 13:15:15 +0530

    authapp created

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "backend.auth",
+    # "rest_framework",
+    # user-define
+    "backend",
+    "authapp",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "acefarm",
+        "USER": "root",
+        "PASSWORD": "password",
+        "PORT": "3306",
+        "HOST": "127.0.0.1",
     }
 }
 
